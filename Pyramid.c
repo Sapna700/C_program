e9bb2b99fd16caea2c58c14613ecb8d1ad9913ea
sapna <sapnasingh00127@gmail.com>
2024-09-12 22:08:02 +0530

    Add files via upload

---
+++ Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h> // For Windows specific gotoxy
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int main() {
+    int num, i, y, x = 40;
+    printf("\nEnter your number for \ngenerating the pyramid:\n");
+    scanf("%d", &num);
+    for (y = 0; y <= num; y++) {
+        gotoxy(x, y + 1);
+        for (i = 0 - y; i <= y; i++)
+            printf("%3d", abs(i));
+        x = x - 3;
+    }
+    return 0;
+}
